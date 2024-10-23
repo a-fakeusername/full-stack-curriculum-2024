a9fb3b3c01341d2b9848633f455420f8594e7408
a-fakeusername <71799509+a-fakeusername@users.noreply.github.com>
2024-10-22 21:40:51 -0500

    react demo

---
+++ Week4/react-demo/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import MyComponent from './MyComponent.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+    </div>
+  );
+}
+
+export default App;
