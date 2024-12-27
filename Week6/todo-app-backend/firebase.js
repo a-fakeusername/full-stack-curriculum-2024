1974f1d2e53ad27d243c1aa5658ed36e7b4ffdb6
a-fakeusername <71799509+a-fakeusername@users.noreply.github.com>
2024-12-26 20:33:59 -0600

    bruh

--- Week6/todo-app-backend/firebase.js
+++ Week6/todo-app-backend/firebase.js
@@ -19,13 +19,13 @@ require("dotenv").config();
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDlb9VzQy1l-JFMI6LS82FLOj9ax_grm24",
-  authDomain: "todo-2ad82.firebaseapp.com",
-  projectId: "todo-2ad82",
-  storageBucket: "todo-2ad82.firebasestorage.app",
-  messagingSenderId: "1004456699335",
-  appId: "1:1004456699335:web:5ec4a1f99e503de8bc65e2",
-  measurementId: "G-1N0QRW3MLJ"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
